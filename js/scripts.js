e3bcc911fa64798814e430ff74cc08371b024b89
deso2die4 <kojic1@live.de>
2022-04-20 04:47:46 +0200

    added 3 pokemons objects to the pokemonlist var

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'crazyPokemon', height: 9, type: ['crazyness','loco'] },
+  {name: 'bigPokemon', height: 30, type: ['bigness','tall'] },
+  {name: 'coolPokemon', height: 12, type: ['coolness','chill'] }
+];
