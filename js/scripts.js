ec1c39db2bc1f53953fa3b86436bc3b0b4bbd2d9
deso2die4 <kojic1@live.de>
2022-04-26 00:44:40 +0200

    added a loop of the pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
   {name: 'bigPokemon', height: 30, type: ['bigness','tall'] },
   {name: 'coolPokemon', height: 12, type: ['coolness','chill'] }
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i]).height > 15) {
+      document.write(pokemonList[i].name+'height:'+pokemonList[i].height+'Wow thats a big height!')
+    else {
+      document.write(pokemonList[i].name+'height:'+pokemonList[i].height)
+    }
+    }
+}
