490a210d538a8917ec3f2d901b7f6a5edbf80fef
deso2die4 <kojic1@live.de>
2022-04-29 04:31:51 +0200

    IIFE changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+let pokemonRepository = (function () {
 let pokemonList = [
   {name: 'crazyPokemon', height: 9, type: ['crazyness','loco'] },
   {name: 'bigPokemon', height: 30, type: ['bigness','tall'] },
@@ -5,11 +6,31 @@ let pokemonList = [
 ];
 
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i]).height > 15) {
-      document.write(pokemonList[i].name+'height:'+pokemonList[i].height+'Wow thats a big height!')
-    else {
-      document.write(pokemonList[i].name+'height:'+pokemonList[i].height)
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+
+    function getAll() {
+      return pokemonList;
     }
-    }
-}
+
+    return {
+    add: add,
+    getAll: getAll
+  };
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'new Pokemon' });
+//for (let i = 0; i < pokemonList.length; i++) {
+  //  if (pokemonList[i]).height > 15) {
+  //    document.write(pokemonList[i].name+'height:'+pokemonList[i].height+'Wow thats a big height!')
+  //  else {
+//      document.write(pokemonList[i].name+'height:'+pokemonList[i].height)
+//    }
+//    }
+//}
+
+pokemonRepository.getALL.forEach(function(name) {
+   console.log(name);
+});
