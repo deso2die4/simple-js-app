15f4051cb5a81f9004ea0b923de465aa99f91af7
deso2die4 <kojic1@live.de>
2022-06-21 17:52:36 +0200

    Updated the Loop from Task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,13 @@
-let pokemonRepository = (function () {
 let pokemonList = [
   {name: 'crazyPokemon', height: 9, type: ['crazyness','loco'] },
   {name: 'bigPokemon', height: 30, type: ['bigness','tall'] },
   {name: 'coolPokemon', height: 12, type: ['coolness','chill'] }
 ];
 
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
+for ( i = 0; i < pokemonList.length; i++) {  //A Loop that goes over each Arraya in the pokemonList 
+  if (pokemonList[i].height > 15) {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " Wow thats big!!!" + "<br>");
+  } else {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + "<br>");
   }
-
-
-    function getAll() {
-      return pokemonList;
-    }
-
-    return {
-    add: add,
-    getAll: getAll
-  };
-})();
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'new Pokemon' });
-//for (let i = 0; i < pokemonList.length; i++) {
-  //  if (pokemonList[i]).height > 15) {
-  //    document.write(pokemonList[i].name+'height:'+pokemonList[i].height+'Wow thats a big height!')
-  //  else {
-//      document.write(pokemonList[i].name+'height:'+pokemonList[i].height)
-//    }
-//    }
-//}
-
-pokemonRepository.getALL.forEach(function(name) {
-   console.log(name);
-});
+}
