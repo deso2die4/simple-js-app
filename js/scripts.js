2e864ed243568bd0b9bf63526436eeea54fdc3ed
deso2die4 <kojic1@live.de>
2022-07-11 20:25:39 +0200

    Exercise 1.8, made a modal and updated design

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,79 @@ let pokemonRepository = (function() {
   button.classList.add('button-design');
   button.addEventListener('click', function(){
     showDetails(pokemon);
+  //  showModal();
   });
   pokemonList.appendChild(pokemonList2);
   pokemonList2.appendChild(button);
   }
+
+// shows on click the Details of the Pokemon in a modal
+  function showModal(titel, content, image){
+    let modalContainer = document.querySelector('.modal-container');
+    modalContainer.classList.remove('invisible');
+    let modal = document.querySelector('.modal')
+    modalContainer.appendChild(modal);
+    let buttonClose = document.createElement('div');
+    buttonClose.classList.add('button-close');
+    let title = modal.querySelector('h1');
+    let text = modal.querySelector('p');
+    title.innerText = titel;
+    text.innerText = 'height: ' + content;
+    let img = document.querySelector('.image');
+    img.src = image;
+    img.classList.remove('invisible');
+
+  }
+// hide function for modal
+function hideModal() {
+  let modalContainer = document.querySelector('.modal-container');
+  modalContainer.classList.add('invisible');
+  let img = document.querySelector('.image');
+  img.classList.add('invisible')
+
+}
+// hides the modal on click of button= close-button
+let closeButton = document.querySelector('.button-close');
+closeButton.addEventListener('click', hideModal);
+// hides modal when press Escape
+window.addEventListener('keydown', (event) => {
+  let modalContainer = document.querySelector('.modal-container');
+  if(event.key === 'Escape' && !modalContainer.classList.contains('.invisible')) {
+    hideModal();
+  }
+});
+// hides modal on click outside the modal
+document.querySelector('.modal-container').addEventListener('click', function (target) {
+  let modalContainer = document.querySelector('.modal-container');
+  if (target.target === modalContainer) {
+    hideModal();
+  }
+})
+
+  /*function showModal(){
+    let modalContainer = document.createElement('div');
+    document.body.appendChild(modalContainer);
+    modalContainer.classList.add('modalContainer');
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    modalContainer.appendChild(modal);
+    let buttonClose = document.createElement('div');
+    buttonClose.classList.add('button-close');
+    let title = document.createElement('h1');
+    modal.appendChild(title);
+    let text = document.createElement('p');
+    modal.appendChild(text);
+    title.innerText = 'hello';
+    console.log('hello');
+  }*/
+
+
+
+
+
+
+
+
 
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -61,7 +130,7 @@ function loadDetails(item){
 //function for addEventListener as a parameter
     function showDetails(pokemon) {
       loadDetails(pokemon).then(function(){
-        console.log(pokemon);
+        showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
       });
     }
 
